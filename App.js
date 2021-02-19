c50d4f0859a47cb9522446367b4d370cd576f2a8
Chase-Zimm <76975306+Chase-Zimm@users.noreply.github.com>
2021-02-19 17:36:32 -0600

    Fix typo

--- App.js
+++ App.js
@@ -11,7 +11,7 @@ const navigator = createStackNavigator(
   {
     initialRouteName: 'HomeScreen',
     defaultNavigationOptions: {
-      title: 'InvertorMe',
+      title: 'InventorMe',
     },
   }
 );

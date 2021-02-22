92553c934f3884dba5552b1b5d872a140c79436c
Estebanadm <70405222+Estebanadm@users.noreply.github.com>
2021-02-22 14:49:32 -0600

    fixing es-lint

--- src/screens/MainPage/mainPage.js
+++ src/screens/MainPage/mainPage.js
@@ -10,12 +10,18 @@ const MainPageNav = () => {
   // Once the user is logged in we will have to get the information from the database using an array of titles just 
   // to have an Idea
   const Tab = createBottomTabNavigator();
-
+  const MainPage = () => {
+    return (
+      <View style={styles.container}>
+        <Text>Main Page</Text>
+      </View>
+    );  
+  };
   return (
-      <Tab.Navigator
-        initialRouteName = "MainPage"
-        backBehavior = "none"
-        tabBarOptions = {{
+    <Tab.Navigator
+      initialRouteName="MainPage"
+      backBehavior="none"
+      tabBarOptions={{
           activeTintColor : "white",
           inactiveTintColor : "#009688",
           activeBackgroundColor : "#009688",
@@ -25,46 +31,38 @@ const MainPageNav = () => {
           },
           showLabel : false
         }}
-      >
-        <Tab.Screen
-          name = "addItem"
-          component = {addItem}
-          options = {{
+    >
+      <Tab.Screen
+        name="addItem"
+        component={addItem}
+        options={{
             tabBarIcon : ({color, size}) => {
-              return (<AntDesign name = "pluscircle" size = {size} color = {color} />);
+              return (<AntDesign name="pluscircle" size={size} color={color} />);
             } 
           }}
-        />
+      />
 
-        <Tab.Screen
-          name = "MainPage"
-          component = {MainPage}
-          options = {{
+      <Tab.Screen
+        name="MainPage"
+        component={MainPage}
+        options={{
             tabBarIcon : ({color, size}) => {
-              return (<FontAwesome name = "home" size = {size} color = {color} />);
+              return (<FontAwesome name="home" size={size} color={color} />);
             }
           }}
-        />
+      />
 
-        <Tab.Screen
-          name = "scanItem"
-          component = {scanItem}
-          options = {{
+      <Tab.Screen
+        name="scanItem"
+        component={scanItem}
+        options={{
             tabBarIcon : ({color, size}) => {
-              return (<Entypo name = "camera" size = {size} color = {color} />)
+              return (<Entypo name="camera" size={size} color={color} />)
             }
           }}
-        />
-      </Tab.Navigator>
+      />
+    </Tab.Navigator>
   );
-};
-
-const MainPage = () => {
-  return (
-    <View style = {styles.container}>
-      <Text>Main Page</Text>
-    </View>
-  );  
 };
 
 export default MainPageNav;

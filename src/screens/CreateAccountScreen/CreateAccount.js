9b09c6ffab4360e15290562b535f0b9fd4511025
AnthonyGuerra7 <lopezguerraanthony@gmail.com>
2021-02-24 06:33:54 -0600

    added db.json and REST method calls need to add Websocket connetion

--- src/screens/CreateAccountScreen/CreateAccount.js
+++ src/screens/CreateAccountScreen/CreateAccount.js
@@ -5,13 +5,60 @@ import { Text, View, Image, StatusBar } from "react-native";
 import { render } from 'react-dom';
 import { FaArrowLeft } from 'react-icons/fa';
 import { FontAwesome } from '@expo/vector-icons';
-
+import { useState, useEffect} from 'react';
 
 
 const CreateAccountScreen = (props) => {
 
     const phoneRegEx = new RegExp('/^[(]{0,1}[0-9]{3}[)]{0,1}[-\s\.]{0,1}[0-9]{3}[-/\s\.]{0,1}[0-9]{4}$/');
-    
+    const [email, setEmail] = useState('')
+    const [firstName, setFirstName] = useState('')
+    const [lastName, setLastName] = useState('')
+    const [phoneNum, setPhoneNum] = useState('')
+    const [password, setPassword] = useState('')
+    const [users, setUsers] = useState([])
+
+    useEffect(() => {
+        const getUsers = async () => {
+          const usersFromServer = await fetchUsers()
+          setUsers(usersFromServer)
+        }
+        getUsers()
+      }, [])
+
+      // Fetch Users
+      const fetchUsers = async () => {
+          const res = await fetch('https://application-mock-server.localtunnel.me/users')
+          const data = await res.json()
+          return data
+      }
+
+      // Fetch User
+      const fetchUser = async (id) => {
+        const res = await fetch(`https://application-mock-server.localtunnel.me/users/${id}`)
+        const data = await res.json()
+        return data
+    }
+    //Add User
+    const addUser = async (user) => {
+        const res = await fetch('https://application-mock-server.localtunnel.me/users',{
+            method: "POST",
+            headers: {
+                'Content-type': 'application/json'
+            },
+            body: JSON.stringify(user)
+
+        })
+        const data = await res.json()
+        setUsers([...users, data])
+    }
+    //Delete User
+    const deleteUser = async (id) => {
+        await fetch(`https://application-mock-server.localtunnel.me/users/${id}`, {
+            method: 'DELETE'
+        })
+        //setUsers(users.filter(user) => user.id != id)
+    }
     return (
         <View style={styles.Page}>
             <View style={styles.arrow}>
@@ -34,6 +81,7 @@ const CreateAccountScreen = (props) => {
                 <TextInput 
                     style={styles.TextInput}
                     placeholder='Email'
+                    value={email} onChange={(e) => setEmail(e.target.value)}
                 />
             </View>
 
@@ -43,6 +91,7 @@ const CreateAccountScreen = (props) => {
                     label="First Name:"
                     style={styles.TextInput}
                     placeholder='First Name'
+                    value={firstName} onChange={(e) => setFirstName(e.target.value)}
                     
                 />
             </View>
@@ -53,6 +102,7 @@ const CreateAccountScreen = (props) => {
                     label="Last Name:"
                     style={styles.TextInput}
                     placeholder='Last Name'
+                    value={lastName} onChange={(e) => setLastName(e.target.value)}
                 />
             </View>
 
@@ -63,6 +113,7 @@ const CreateAccountScreen = (props) => {
                     style={styles.TextInput}
                     placeholder='Phone Number'
                     validations={{matchRegexp:phoneRegEx}}
+                    value={phoneNum} onChange={(e) => setPhoneNum(e.target.value)}
                 />
             </View>
 
@@ -73,12 +124,13 @@ const CreateAccountScreen = (props) => {
                     secureTextEntry
                     style={styles.TextInput}
                     placeholder='Password'
+                    value={password} onChange={(e) => setPassword(e.target.value)}
                     
                 />
             </View>
             <View style={styles.logo}>
                 <TouchableOpacity
-                    style={styles.appButtonContainer} onPress={()=>props.navigation.navigate("MainPage")}>
+                    style={styles.appButtonContainer} onPress={()=>{{addUser};props.navigation.navigate("MainPage");}}>
                     <Text style={styles.appButtonText}>Create Account</Text>
                 </TouchableOpacity>
             </View> 

3ed593ce22dd4d611e6a11259e21d7a2474875fb
AnthonyGuerra7 <lopezguerraanthony@gmail.com>
2021-02-23 21:31:11 -0600

    updated styles and added back button functionality

--- src/screens/CreateAccountScreen/CreateAccount.js
+++ src/screens/CreateAccountScreen/CreateAccount.js
@@ -1,8 +1,11 @@
 import React from 'react'
 import { TextInput, TouchableOpacity } from "react-native-gesture-handler";
-import styles from "../LogInScreen/LogIn.style";
+import styles from "../CreateAccountScreen/CreateAccountScreen.style";
 import { Text, View, Image, StatusBar } from "react-native";
 import { render } from 'react-dom';
+import { FaArrowLeft } from 'react-icons/fa';
+import { FontAwesome } from '@expo/vector-icons';
+
 
 
 const CreateAccountScreen = (props) => {
@@ -10,51 +13,78 @@ const CreateAccountScreen = (props) => {
     const phoneRegEx = new RegExp('/^[(]{0,1}[0-9]{3}[)]{0,1}[-\s\.]{0,1}[0-9]{3}[-/\s\.]{0,1}[0-9]{4}$/');
     
     return (
-        
+        <View style={styles.Page}>
+            <View style={styles.arrow}>
+                
+                <TouchableOpacity
+                    style={styles.arrowButtonContainer} onPress={()=>props.navigation.navigate("HomeScreen")}>
+                    <FontAwesome name='arrow-left' color='#009688' size={45}>
+                    </FontAwesome>
+                </TouchableOpacity>
+                
+            </View>
         <View style={styles.container}>
-            
-            
-            <Image source={require('../../../assets/appImages/InventorMELogo.png')} />
-            <Text style={{color: '#009688'}}>Email:</Text>
-            <TextInput 
-                label="Email:"
-                style={styles.TextInput}
-                placeholder='Email'
-            />
-            <Text style={{color: '#009688'}} >First Name:</Text>
-            <TextInput
-                label="First Name:"
-                style={styles.TextInput}
-                placeholder='First Name'
                 
-            />
-            <Text style={{color: '#009688', }}>Last Name:</Text>
-            <TextInput 
-                label="Last Name:"
-                style={styles.TextInput}
-                placeholder='Last Name'
-            />
-            <Text style={{color: '#009688'}}>Phone Number:</Text>
-            <TextInput 
-                label="Phone Number::"
-                style={styles.TextInput}
-                placeholder='Phone Number'
-                validations={{matchRegexp:phoneRegEx}}
-            />
-            <Text style={{color: '#009688'}}>Password:</Text>
-            <TextInput
-                label="Password:"
-                secureTextEntry
-                style={styles.TextInput}
-                placeholder='Password'
-                
-            />
-            <TouchableOpacity
-                style={styles.appButtonContainer} onPress={()=>props.navigation.navigate("MainPage")}>
-                <Text style={styles.appButtonText}>Create Account</Text>
-        </TouchableOpacity>
+            
+            <View style={styles.logo}>
+                <Image source={require('../../../assets/appImages/InventorMELogo.png')} />
+            </View>
+            <View style={styles.child}>
+                <Text style={{color: '#009688'}}>Email:</Text>
+                <TextInput 
+                    style={styles.TextInput}
+                    placeholder='Email'
+                />
+            </View>
+
+            <View style={styles.child}>
+                <Text style={{color: '#009688'}} >First Name:</Text>
+                <TextInput
+                    label="First Name:"
+                    style={styles.TextInput}
+                    placeholder='First Name'
+                    
+                />
+            </View>
+
+            <View style={styles.child}>
+                <Text style={{color: '#009688', }}>Last Name:</Text>
+                <TextInput 
+                    label="Last Name:"
+                    style={styles.TextInput}
+                    placeholder='Last Name'
+                />
+            </View>
+
+            <View style={styles.child}>
+                <Text style={{color: '#009688'}}>Phone Number:</Text>
+                <TextInput 
+                    label="Phone Number::"
+                    style={styles.TextInput}
+                    placeholder='Phone Number'
+                    validations={{matchRegexp:phoneRegEx}}
+                />
+            </View>
+
+            <View style={styles.child}>
+                <Text style={{color: '#009688'}}>Password:</Text>
+                <TextInput
+                    label="Password:"
+                    secureTextEntry
+                    style={styles.TextInput}
+                    placeholder='Password'
+                    
+                />
+            </View>
+            <View style={styles.logo}>
+                <TouchableOpacity
+                    style={styles.appButtonContainer} onPress={()=>props.navigation.navigate("MainPage")}>
+                    <Text style={styles.appButtonText}>Create Account</Text>
+                </TouchableOpacity>
+            </View> 
 
         </View>
+        </View> 
     )
     
 }
--- src/screens/CreateAccountScreen/CreateAccountScreen.style.js
+++ src/screens/CreateAccountScreen/CreateAccountScreen.style.js
@@ -1,13 +1,38 @@
 import {StyleSheet} from 'react-native';
 
 export default StyleSheet.create({
+    Page:{
+      flex:1,
+      alignItems: 'stretch',
+    },
+    child: {
+      
+      alignItems: 'flex-start',
+      justifyContent: 'center'
+      
+    },
+    arrow: {
+      alignItems:'flex-start',
+      justifyContent: 'flex-end',
+      paddingTop: 50,
+      paddingStart: 30,
+      backgroundColor: 'lightgray',
+      
+    },
     container: {
         flex:1,
         backgroundColor:'#fff',
-        alignItems:'center',
-        justifyContent:'center',
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: 'lightgray'
         
       },
+      logo: {
+        
+       
+        
+      },
+
       Image:{
         height:70,
         width: 50,
@@ -18,14 +43,18 @@ export default StyleSheet.create({
         padding: 8,
         margin: 10,
         width: 200,
+        backgroundColor: '#7FFFD4'
       },
-      Text:{
-        alignItems: "flex-start"
-        
-      },
+      
       appButtonContainer: {
         elevation: 8,
         backgroundColor: "#009688",
+        borderRadius: 10,
+        paddingVertical: 10,
+        paddingHorizontal: 12
+      },
+      arrowButtonContainer: {
+        elevation: 8,
         borderRadius: 10,
         paddingVertical: 10,
         paddingHorizontal: 12

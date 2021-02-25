ebdf0c3c9ca95369c43e5acdbee86608500af029
Estebanadm <70405222+Estebanadm@users.noreply.github.com>
2021-02-25 08:25:42 -0600

    Upper tab created
    Missing functionality

--- src/Components/UpperTab.js
+++ src/Components/UpperTab.js
@@ -1,13 +1,27 @@
 /* eslint-disable no-use-before-define */
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, Touchable } from "react-native";
+import { MaterialCommunityIcons, FontAwesome,FontAwesome5, Entypo } from "@expo/vector-icons";
+import { TouchableOpacity } from "react-native-gesture-handler";
 
 
 
 const ProfilePage = props => {
     return (
       <View style={styles.container}>
-        <Text style={styles.textStyle}>{props.title}</Text>
+        <View style={styles.buttonLeftStyle}>
+          <TouchableOpacity>
+            <FontAwesome5 name="hamburger" size={30} color='black' />
+          </TouchableOpacity>
+        </View>
+        <View style={{alignItems:'center'}}>
+          <Text style={styles.textStyle}>{props.title}</Text>
+        </View>
+        <View style={styles.buttonRightStyle}>
+          <TouchableOpacity>
+            <MaterialCommunityIcons name='face-profile' size={30} color='black' />
+          </TouchableOpacity>
+        </View>
       </View>
     );
 }
@@ -15,14 +29,26 @@ const styles = StyleSheet.create({
     container : {
         flex:1,
         backgroundColor : '#009688',
-        alignItems : 'center',
-        justifyContent : 'flex-end',
+        flexDirection:'row',
+        justifyContent:'space-between',
+        alignItems:'flex-end',
     },
     textStyle:{
         color: 'black',
         fontSize:20,
         fontWeight:"bold",
-        height:35,
+        height:30,
+        alignItems:"stretch",
     },
+    buttonLeftStyle:{
+      alignItems:'flex-start',
+      padding:5,
+      left:10,
+    },
+    buttonRightStyle:{
+      alignItems:'flex-start',
+      padding:5,
+      right:10,
+    }      
 });
 export default ProfilePage;
--- src/screens/MainPage/mainPage.js
+++ src/screens/MainPage/mainPage.js
@@ -2,7 +2,6 @@ import React from "react";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { AntDesign, FontAwesome, Entypo } from "@expo/vector-icons";
 import { View, Text } from "react-native";
-import { NavigationContainer } from "@react-navigation/native";
 import styles from "./mainPage.style";
 import addItem from "../addItemScreen/addItem";
 import scanItem from "../scanItemScreen/scanItem";
@@ -11,10 +10,12 @@ import UpperTab from "../../Components/UpperTab";
 const MainPageNav = () => {
   // Once the user is logged in we will have to get the information from the database using an array of titles just 
   // to have an Idea
+  const Tab = createBottomTabNavigator();
+  const MainPage = () => {
     return (
       <View style={{flex:1}}>
         <View style={{flex:0.1}}>
-          <UpperTab title="MainPage" />
+          <UpperTab title="Main Page" />
         </View>
         <View style={styles.container}>
           <Text>Main Page</Text>
@@ -22,5 +23,51 @@ const MainPageNav = () => {
       </View>
     );  
   };
-
+    return (
+      <Tab.Navigator
+        initialRouteName="MainPage"
+        backBehavior="none"
+        tabBarOptions={{
+            activeTintColor : "white",
+            inactiveTintColor : "#009688",
+            activeBackgroundColor : "#009688",
+            inactiveBackgroundColor : "white",
+            style : {
+              borderTopColor : "white"
+            },
+            showLabel : false
+          }}
+      >
+        <Tab.Screen
+          name="addItem"
+          component={addItem}
+          options={{
+              tabBarIcon : ({color, size}) => {
+                return (<AntDesign name="pluscircle" size={size} color={color} />);
+              } 
+            }}
+        />
+  
+        <Tab.Screen
+          name="MainPage"
+          component={MainPage}
+          options={{
+              tabBarIcon : ({color, size}) => {
+                return (<FontAwesome name="home" size={size} color={color} />);
+              }
+            }}
+        />
+  
+        <Tab.Screen
+          name="scanItem"
+          component={scanItem}
+          options={{
+              tabBarIcon : ({color, size}) => {
+                return (<Entypo name="camera" size={size} color={color} />)
+              }
+            }}
+        />
+      </Tab.Navigator>
+    );
+  };
 export default MainPageNav;

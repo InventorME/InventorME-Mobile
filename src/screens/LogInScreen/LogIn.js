59eb2ff9916177c37c5beb06f6b6be17b2183f30
Luke Miller <lukelmiller@icloud.com>
2021-03-05 17:32:33 -0600

    Test

--- src/screens/LogInScreen/LogIn.js
+++ src/screens/LogInScreen/LogIn.js
@@ -13,7 +13,7 @@ const HomeScreen = (props) => {
   const [password, setPassword] = useState("");
 
   const onSubmit = ()=> {
-    preventDefault();
+    //preventDefault();
 
     const user = new CognitoUser({
       Username: email,

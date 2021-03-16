03d71af865c2c2019ef6e1db65b2bb750c756a51
Chase-Zimm <76975306+Chase-Zimm@users.noreply.github.com>
2021-03-15 23:29:15 -0500

    add header for items screen

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import React from "react";
 import { createStackNavigator } from "@react-navigation/stack";
 import { NavigationContainer } from "@react-navigation/native";
 import { Account } from './src/util/Accounts';
+import { FontAwesome } from "@expo/vector-icons";
 import HomeScreen from "./src/screens/LogInScreen/LogIn";
 import MainPageNav from "./src/screens/MainPage/mainPage";
 import CreateAccountScreen from "./src/screens/CreateAccountScreen/CreateAccount";
@@ -21,35 +22,58 @@ function myStack() {
       <Account>
       <Stack.Navigator 
         initialRouteName="HomeScreen"
-        headerMode="none"
       >
         <Stack.Screen
           name="HomeScreen"
           component={HomeScreen}
+          options = {{
+            headerShown : false
+          }}
         />
         <Stack.Screen
           name="MainPage"
           component={MainPageNav}
+          options = {{
+            headerShown : false
+          }}
         />
         <Stack.Screen
           name="CreateAccountScreen"
           component={CreateAccountScreen}
+          options = {{
+            headerShown : false
+          }}
         />
         <Stack.Screen
           name="ProfilePage"
           component={ProfilePageNav}
+          options = {{
+            headerShown : false
+          }}
         />
         <Stack.Screen
           name="AddItemScreen"
           component={addItemScreen}
+          options = {{
+            headerShown : false
+          }}
         />
         <Stack.Screen
           name="EditProfilePage"
           component={EditProfilePage}
+          options = {{
+            headerShown : false
+          }}
         />
         <Stack.Screen
           name = "ItemsScreen"
           component = {Items}
+          options = {{
+            headerStyle : {backgroundColor : "#009688"},
+            headerBackImage : () => {return <FontAwesome name="arrow-left" color="white" size={25}/>},
+            headerBackTitleVisible : false,
+            headerLeftContainerStyle : {marginLeft : "2%"}
+          }}
         />
       </Stack.Navigator>
       </Account>

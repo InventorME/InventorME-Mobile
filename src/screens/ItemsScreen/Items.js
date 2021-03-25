2aaf26e2f669aae1fa8ce7c57ec29c89ac7b4123
Chase-Zimm <76975306+Chase-Zimm@users.noreply.github.com>
2021-03-25 14:52:52 -0500

    Create items screen

--- src/screens/ItemsScreen/Items.js
+++ src/screens/ItemsScreen/Items.js
@@ -2,16 +2,20 @@ import React from "react";
 import { View, Text } from "react-native";
 import styles from "./Items.style";
 import { TouchableOpacity } from "react-native-gesture-handler";
+import BoxFolderComponent from "../../Components/BoxFolderComponent";
 
 const Items = (props) => {
     return (
       <View style={styles.container}>
-        <Text>Items</Text>
-        <TouchableOpacity
-                style={styles.addItemContainer}
-                onPress={()=>{props.navigation.navigate("AddItemScreen")}}>
-            <Text>Add Item</Text>
-        </TouchableOpacity>
+        <View style = {styles.boxRow}>
+          <BoxFolderComponent
+            title = "Item 1"
+          />
+          
+          <BoxFolderComponent
+            title = "Item 2"
+          />
+        </View>
       </View>
     );
 }
--- src/screens/ItemsScreen/Items.style.js
+++ src/screens/ItemsScreen/Items.style.js
@@ -3,15 +3,20 @@ import {StyleSheet} from 'react-native';
 export default StyleSheet.create({
     container : {
         flex : 1,
-        backgroundColor : '#fff',
-        alignItems : 'center',
-        justifyContent : 'center',
+        backgroundColor : 'white',
+        alignItems : 'flex-start',
+        justifyContent : 'flex-start',
+    },
+    boxRow : {
+        flex : .1,
+        flexDirection : 'row',
+        flexWrap : 'wrap',
     },
     addItemContainer: {
         elevation: 8,
         backgroundColor: "#009688",
         borderRadius: 10,
         paddingVertical: 10,
-        paddingHorizontal: 12
+        paddingHorizontal: 12,
       }
 });
